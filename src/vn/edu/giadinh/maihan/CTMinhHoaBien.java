6064b7236b77a41866e917684952d7478f4de8cf
MaiNg0cHan <maingochan005@gmail.com>
2021-09-27 12:33:30 +0700

    initial commit

---
+++ src/vn/edu/giadinh/maihan/CTMinhHoaBien.java
@@ -0,0 +1,25 @@
+package vn.edu.giadinh.maihan;
+
+public class CTMinhHoaBien {
+    
+
+
+    public static void main(String[] args) {
+        
+        System.out.print("Mai Ngọc Hân" + "Hello" + " Xin Chào");
+        System.out.println(5 + 8);
+
+        float diemKTLapTrinh;
+
+
+        //biến tham chiếu
+        Scanner bienNhapLieu;//Điều khiển từ xa
+
+        bienNhapLieu = new Scanner(System.in);
+
+        System.out.print("Nhập vào điểm môn kỹ thuật lập trình:");
+        diemKTLapTrinh = bienNhapLieu.nextFloat();
+
+    }
+            
+}
